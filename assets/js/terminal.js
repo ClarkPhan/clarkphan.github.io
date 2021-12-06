136d60df51350d0b8210cf38eecb28ccfc9a3257
Clark Phan <clarkphan24@gmail.com>
2021-12-06 01:19:38 -0800

    Add additional terminal commands

--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -1,29 +1,48 @@
 jQuery(function ($, undefined) {
 	$('#terminal').terminal({
-		help: function () {
-			this.echo('Type any of these commands to try them out! Type `help` to see this list again.'
+		help: function() {
+			this.echo(
+				'Type any of these commands to try them out! Type `help` to see this list again.'
 				+ '\nclear'
-				+ '\nabout',
+				+ '\nabout'
+				+ '\nresume'
+				+ '\ncontact',
 				{ keepWords: true }
 			);
 		},
-		about: function () {
-			this.echo('Hey, I\'m Clark! I recently graduated from UCSD and am interested in full-stack web development.'
-				+ '\nI am passionate about creating quality and impactful software that aims to make people\'s lives better.'
-				+ '\nIn my free time I like to play video games and ride my road bike.',
+		about: function() {
+			this.echo(
+				'Hey, I\'m Clark! I recently graduated from UCSD and am passionate about web development.'
+				+ '\n\nSome topics that I find really interesting and exciting at the moment are progressive web apps, the possibilty of Web 3.0,'
+				+ ' and full-stack web development.'
+				+ '\n\nWithin my lifetime, I hope to create quality, impactful, and accessible software that can make people\'s lives better.'
+				+ '\n\nIn my free time, I like to play video games and ride my road bike.',
 				{ keepWords: true }
 			);
 		},
+		resume: function() {
+			this.echo(
+				'Check out my resume [[!;;;;https://clarkphan.com/resume.pdf]here]!',
+				{ keepWords: true }
+			);
+		},
+		contact: function() {
+			this.echo(
+				'Feel free to contact me at [[!;;;;clark@clarkphan.com]clark@clarkphan.com] with any questions!',
+				{ keepWords: true }
+			);
+		}
 	}, {
 		greetings: false,
-		onInit: function () {
-			this.echo('Welcome to my personal website! Please type `help` and then the \'Enter\' key to begin.',
+		onInit: function() {
+			this.echo(
+				'Welcome to my personal website! Please type `help` and then the \'Enter\' key to begin.',
 				{ keepWords: true }
 			);
 		},
 		name: 'Clark\'s Terminal',
 		height: 250,
-		width: 750,
+		width: 650,
 		prompt: 'clark@clarkphan.com ~ $ ',
 		convertLinks: false,
 	});
